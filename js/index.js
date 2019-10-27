b501316630475a1821007efe339182a0102706ab
anwar <weissman1505199@gmail.com>
2019-10-27 23:54:19 +0700

    yeay

--- js/index.js
+++ js/index.js
@@ -1,25 +1,87 @@
 (function() {
 
-  glUtils.SL.init({ callback:function() { main(); } });
+  glUtils.SL.init({ callback: function() { main(); } });
 
   function main() {
-
-    // glUtils.SL.sourceFromHtml();
-    var VSHADER_SOURCE = glUtils.SL.Shaders.v1.vertex;
-    var FSHADER_SOURCE = glUtils.SL.Shaders.v1.fragment;
-
-      
     var canvas = document.getElementById("glcanvas");
     var gl = glUtils.checkWebGL(canvas);
-
-
-    var vertexShader = glUtils.getShader(gl ,gl.VERTEX_SHADER, VSHADER_SOURCE);
-    var fragmentShader = glUtils.getShader(gl , gl.FRAGMENT_SHADER , FSHADER_SOURCE);
-    var program = glUtils.createProgram(gl, vertexShader , fragmentShader);
+  
+    // Inisialisasi shaders dan program
+    var vertexShader = glUtils.getShader(gl, gl.VERTEX_SHADER, glUtils.SL.Shaders.v1.vertex);
+    var fragmentShader = glUtils.getShader(gl, gl.FRAGMENT_SHADER, glUtils.SL.Shaders.v1.fragment);
+    var program = glUtils.createProgram(gl, vertexShader, fragmentShader);
     gl.useProgram(program);
+  
+    // Definisi verteks dan buffer
+    /**
+     *  A ( -0.5, -0.5,  0.5 )
+     *  B ( -0.5,  0.5,  0.5 )
+     *  C (  0.5,  0.5,  0.5 )
+     *  D (  0.5, -0.5,  0.5 )
+     *  E ( -0.5, -0.5, -0.5 )
+     *  F ( -0.5,  0.5, -0.5 )
+     *  G (  0.5,  0.5, -0.5 )
+     *  H (  0.5, -0.5, -0.5 )
+     */
+    var cubeVertices = [
+      // x, y, z            r, g, b
+      -0.5,  0.5,  0.5,     1.0, 0.0, 0.0,  // depan, merah, BAD BDC
+      -0.5, -0.5,  0.5,     1.0, 0.0, 0.0,
+       0.5, -0.5,  0.5,     1.0, 0.0, 0.0,
+      -0.5,  0.5,  0.5,     1.0, 0.0, 0.0,
+       0.5, -0.5,  0.5,     1.0, 0.0, 0.0,
+       0.5,  0.5,  0.5,     1.0, 0.0, 0.0,
+       0.5,  0.5,  0.5,     0.0, 1.0, 0.0,  // kanan, hijau, CDH CHG
+       0.5, -0.5,  0.5,     0.0, 1.0, 0.0,
+       0.5, -0.5, -0.5,     0.0, 1.0, 0.0,
+       0.5,  0.5,  0.5,     0.0, 1.0, 0.0,
+       0.5, -0.5, -0.5,     0.0, 1.0, 0.0,
+       0.5,  0.5, -0.5,     0.0, 1.0, 0.0,
+       0.5, -0.5,  0.5,     0.0, 0.0, 1.0,  // bawah, biru, DAE DEH
+      -0.5, -0.5,  0.5,     0.0, 0.0, 1.0,
+      -0.5, -0.5, -0.5,     0.0, 0.0, 1.0,
+       0.5, -0.5,  0.5,     0.0, 0.0, 1.0,
+      -0.5, -0.5, -0.5,     0.0, 0.0, 1.0,
+       0.5, -0.5, -0.5,     0.0, 0.0, 1.0,
+      -0.5, -0.5, -0.5,     1.0, 1.0, 0.0,  // belakang, kuning, EFG EGH
+      -0.5,  0.5, -0.5,     1.0, 1.0, 0.0,
+       0.5,  0.5, -0.5,     1.0, 1.0, 0.0,
+      -0.5, -0.5, -0.5,     1.0, 1.0, 0.0,
+       0.5,  0.5, -0.5,     1.0, 1.0, 0.0,
+       0.5, -0.5, -0.5,     1.0, 1.0, 0.0,
+      -0.5,  0.5, -0.5,     0.0, 1.0, 1.0,  // kiri, cyan, FEA FAB
+      -0.5, -0.5, -0.5,     0.0, 1.0, 1.0,
+      -0.5, -0.5,  0.5,     0.0, 1.0, 1.0,
+      -0.5,  0.5, -0.5,     0.0, 1.0, 1.0,
+      -0.5, -0.5,  0.5,     0.0, 1.0, 1.0,
+      -0.5,  0.5,  0.5,     0.0, 1.0, 1.0,
+       0.5,  0.5, -0.5,     1.0, 0.0, 1.0,  // atas, magenta, GFB GBC
+      -0.5,  0.5, -0.5,     1.0, 0.0, 1.0,
+      -0.5,  0.5,  0.5,     1.0, 0.0, 1.0,
+       0.5,  0.5, -0.5,     1.0, 0.0, 1.0,
+      -0.5,  0.5,  0.5,     1.0, 0.0, 1.0,
+       0.5,  0.5,  0.5,     1.0, 0.0, 1.0
+    ];
 
-    n = initBuffers(gl, program);
+    var cubeVBO = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVBO);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cubeVertices), gl.STATIC_DRAW);
 
+    var vPosition = gl.getAttribLocation(program, 'vPosition');
+    var vColor = gl.getAttribLocation(program, 'vColor');
+    gl.vertexAttribPointer(
+      vPosition,  // variabel yang memegang posisi attribute di shader
+      3,          // jumlah elemen per attribute
+      gl.FLOAT,   // tipe data atribut
+      gl.FALSE,
+      6 * Float32Array.BYTES_PER_ELEMENT, // ukuran byte tiap verteks 
+      0                                   // offset dari posisi elemen di array
+    );
+    gl.vertexAttribPointer(vColor, 3, gl.FLOAT, gl.FALSE, 
+      6 * Float32Array.BYTES_PER_ELEMENT, 3 * Float32Array.BYTES_PER_ELEMENT);
+
+    gl.enableVertexAttribArray(vPosition);
+    gl.enableVertexAttribArray(vColor);
 
     var thetaLoc = gl.getUniformLocation(program, 'theta');
     var theta = [0.0, 0.0, 0.0];
@@ -39,6 +101,32 @@
     }
     document.addEventListener('keypress', onKeyPress);
 
+    // Definisi view dan projection
+    var vmLoc = gl.getUniformLocation(program, 'view');
+    var pmLoc = gl.getUniformLocation(program, 'projection');
+    var vm = glMatrix.mat4.create();
+    var pm = glMatrix.mat4.create();
+
+    glMatrix.mat4.lookAt(vm,
+      glMatrix.vec3.fromValues(0.0, 0.0, 0.0),    // posisi kamera
+      glMatrix.vec3.fromValues(0.0, 0.0, -2.0),  // titik yang dilihat; pusat kubus akan kita pindah ke z=-2
+      glMatrix.vec3.fromValues(0.0, 1.0, 0.0)   // arah atas dari kamera
+    );
+
+    var fovy = glMatrix.glMatrix.toRadian(90.0);
+    var aspect = canvas.width / canvas.height;
+    var near = 0.5;
+    var far = 10.0;
+    glMatrix.mat4.perspective(pm,
+      fovy,
+      aspect,
+      near,
+      far
+    );
+
+    gl.uniformMatrix4fv(vmLoc, false, vm);
+    gl.uniformMatrix4fv(pmLoc, false, pm);
+
     function render() {
       
       theta[axis] += 0.5;  // dalam derajat
@@ -50,104 +138,10 @@
       gl.drawArrays(gl.TRIANGLES, 0, 36);
       requestAnimationFrame(render); 
     }
-
     // Bersihkan layar jadi hitam
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.enable(gl.DEPTH_TEST);
     render();
   }
 
-  function initBuffers(gl, program) {
-    // var triangleVertices=new Float32Array([
-    //   //x,y   r,g,b
-    //   0.0, 0.5,   1.0, 1.0, 0.0,
-    //   -0.5, -0.5,  0.7, 0.0, 1.0,
-    //   0.5, -0.5,  0.1, 1.0, 0.6,
-    // ]);
-
-    var cubeVertice = new Float32Array( [
-      -0.5, 0.5, 0.5,  1.0, 0.0, 0.0,
-      -0.5, -0.5, 0.5,  1.0, 0.0, 0.0,
-      0.5, -0.5, 0.5,  1.0, 0.0, 0.0,
-
-      -0.5, 0.5, 0.5,  1.0, 0.0, 0.0,
-      0.5, -0.5, 0.5,  1.0, 0.0, 0.0,
-      0.5, 0.5, 0.5,   1.0, 0.0, 0.0,
-
-
-      0.5, 0.5, 0.5,   0.0, 1.0, 0.0,
-      0.5, -0.5, 0.5,  0.0, 1.0, 0.0,
-      0.5, -0.5, -0.5,  0.0, 1.0, 0.0,
-
-      0.5, 0.5, 0.5,   0.0, 1.0, 0.0,
-      0.5, -0.5, -0.5,  0.0, 1.0, 0.0,
-      0.5, 0.5, -0.5,  0.0, 1.0, 0.0,
-
-
-      0.5, -0.5, 0.5,  0.0, 0.0, 1.0,
-      -0.5, -0.5, 0.5,  0.0, 0.0, 1.0,
-      -0.5, -0.5, -0.5,  0.0, 0.0, 1.0,
-
-      0.5, -0.5, 0.5,  0.0, 0.0, 1.0,
-      -0.5, -0.5, -0.5,  0.0, 0.0, 1.0,
-      0.5, -0.5, -0.5,  0.0, 0.0, 1.0,
-
-
-      -0.5, -0.5, -0.5,  1.0, 1.0, 0.0,
-      -0.5, 0.5, -0.5,  1.0, 1.0, 0.0,
-      0.5, 0.5, -0.5,  1.0, 1.0, 0.0,
-
-      -0.5, -0.5, -0.5,  1.0, 1.0, 0.0,
-      0.5, 0.5, -0.5,  1.0, 1.0, 0.0,
-      0.5, -0.5, -0.5,  1.0, 1.0, 0.0,
-
-
-      -0.5, 0.5, -0.5,  0.0, 1.0, 1.0,
-      -0.5, -0.5, -0.5,  0.0, 1.0, 1.0,
-      -0.5, -0.5, 0.5,  0.0, 1.0, 1.0,
-
-      -0.5, 0.5, -0.5,  0.0, 1.0, 1.0,
-      -0.5, -0.5, 0.5,  0.0, 1.0, 1.0,
-      -0.5, 0.5, 0.5,  0.0, 1.0, 1.0,
-
-
-      0.5, 0.5, -0.5,  1.0, 0.0, 1.0,
-      -0.5, 0.5, -0.5,  1.0, 0.0, 1.0,
-      -0.5, 0.5, 0.5,  1.0, 0.0, 1.0,
-
-      0.5, 0.5, -0.5,  1.0, 0.0, 1.0,
-      -0.5, 0.5, 0.5,  1.0, 0.0, 1.0,
-      0.5, 0.5, 0.5,   1.0, 0.0, 1.0,
-      
-    ]);
-
-    var n = 36;
-
-    var vertexBuffer = gl.createBuffer();
-    if (!vertexBuffer) {
-      console.log('Failed to create the buffer object');
-      return -1;
-    }
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cubeVertice), gl.STATIC_DRAW);
-
-    var vPosition = gl.getAttribLocation(program, 'vPosition');
-    var vColor = gl.getAttribLocation(program, 'vColor');
-    
-    gl.vertexAttribPointer(
-      vPosition , 3 , gl.FLOAT, gl.FALSE, 6 * Float32Array.BYTES_PER_ELEMENT , 0
-    );
-
-    gl.vertexAttribPointer(
-      vColor , 3 , gl.FLOAT, gl.FALSE, 6 * Float32Array.BYTES_PER_ELEMENT , 3 * Float32Array.BYTES_PER_ELEMENT
-    );
-
-    gl.enableVertexAttribArray(vPosition);
-    gl.enableVertexAttribArray(vColor);
-
-    return n;
-  }
-   
 })();
-
-
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -4,9 +4,17 @@ attribute vec3 vPosition;
 attribute vec3 vColor;
 varying vec3 fColor;
 uniform vec3 theta;
+uniform mat4 projection;
+uniform mat4 view;
 
 void main() {
   fColor = vColor;
+  mat4 translate = mat4(
+    1.0, 0.0, 0.0, 0.0,
+    0.0, 1.0, 0.0, 0.0,
+    0.0, 0.0, 1.0, 0.0,
+    0.0, 0.0, -2.0, 1.0         // Kita geser setiap verteks sejau 2 unit menjauhi kamera, untuk memastikan seluruh bagian kubus ada di antara near dan far.
+  );
   vec3 angle = radians(theta);
   vec3 c = cos(angle);
   vec3 s = sin(angle);
@@ -28,6 +36,6 @@ void main() {
     0.0, 0.0, 1.0, 0.0,
     0.0, 0.0, 0.0, 1.0
   );
-  gl_Position = rz * ry * rx * vec4(vPosition, 1.0);
-
+  gl_Position = projection * view * translate * rz * ry * rx * vec4(vPosition, 1.0);
+  // urutan perkaliannya harus = projection x view x model (transformasi)
 }
